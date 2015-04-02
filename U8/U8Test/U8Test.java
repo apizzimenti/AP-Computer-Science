d625f359d277bd07a7a2b1c14d06b080bef8a579
Anthony Pizzimenti <pizzimentianthony@gmail.com>
2015-04-02 10:21:44 -0500

    spacing change

--- U8/U8Test/U8Test.java
+++ U8/U8Test/U8Test.java
@@ -10,46 +10,46 @@ import java.io.*;
 import java.awt.*;
 
 public class U8Test extends JFrame {
-
+    
     private ArrayList<player> players = new ArrayList<player>();
     private Container contain = getContentPane();
     private JTextArea area = new JTextArea();
-
+    
     public U8Test() {
         contain.add(area);
         Font except = new Font("monospaced", Font.PLAIN, 14);
         area.setFont(except);
-		generate();
-		area.append("Original List\n\n");
-		printList();
-		area.append("\n\n");
-		sort();
-		area.append("Sorted List\n\n");
+        generate();
+        area.append("Original List\n\n");
+        printList();
+        area.append("\n\n");
+        sort();
+        area.append("Sorted List\n\n");
         printList();
     }
-
+    
     public void generate() {
         try {
             Scanner reader = new Scanner(new File(
-                "C:\\Users\\westsh\\Dropbox\\Comp Sci\\U8\\U8Test\\u7test.txt"
-                ));
-
+               "C:\\Users\\westsh\\Dropbox\\Comp Sci\\U8\\U8Test\\u7test.txt"
+               ));
+            
             while(reader.hasNext()) {
                 players.add(new player(reader.nextLine()));
-			}
+            }
         }
         catch(IOException e) {
             throw new RuntimeException(e.toString());
         }
     }
-
+    
     public static void main(String[]args) {
         U8Test x = new U8Test();
         x.setSize(600,800);
         x.setVisible(true);
         x.setDefaultCloseOperation(DISPOSE_ON_CLOSE);
     }
-
+    
     public void printList() {
         area.append("Number\tYear\tPosition\tPoint Per Game\n");
         area.append("------\t----\t--------\t--------------\n");
@@ -61,21 +61,21 @@ public class U8Test extends JFrame {
             area.append("     " + player.getPoints() + "\n");
         }
     }
-
+    
     public void sort() {
-		Double temp;
-		int f;
-
-		for(int i = 1; i < players.size(); i++) {
-			player x = players.get(i);
-			temp = x.getPoints();
-			f = i;
-			while (f > 0 && temp > players.get(f-1).getPoints()) {
-				player y = players.get(f-1);
-				players.set(f, y);
-				f--;
-			}
-			players.set(f, x);
-		}
+        Double temp;
+        int f;
+        
+        for(int i = 1; i < players.size(); i++) {
+            player x = players.get(i);
+            temp = x.getPoints();
+            f = i;
+            while (f > 0 && temp > players.get(f-1).getPoints()) {
+                player y = players.get(f-1);
+                players.set(f, y);
+                f--;
+            }
+            players.set(f, x);
+        }
     }
 }
--- U8/U8Test/player.java
+++ U8/U8Test/player.java
@@ -4,32 +4,32 @@
 /*  ---------------------- */
 
 public class player {
-
+    
     private String num;
     private String year;
     private String pos;
     private double points;
-
+    
     public player(String line) {
-		String[]data = line.split(" ");
-		num = data[0];
-		year = data[1];
-		pos = data[2];
-		points = Double.parseDouble(data[3]);
+        String[]data = line.split(" ");
+        num = data[0];
+        year = data[1];
+        pos = data[2];
+        points = Double.parseDouble(data[3]);
     }
-
+    
     public String getNum() {
-		return num;
+        return num;
     }
-
+    
     public String getYear() {
         return year;
     }
-
+    
     public String getPos() {
         return pos;
     }
-
+    
     public double getPoints() {
         return points;
     }
