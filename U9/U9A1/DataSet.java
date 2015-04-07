792942edf1228a858f31e6e66bd361a5dd9ce923
apizzimenti <pizzimentianthony@gmail.com>
2015-04-07 18:06:01 -0500

    init

---
+++ U9/U9A1/DataSet.java
@@ -0,0 +1,44 @@
+public class DataSet {
+
+    private static double max;
+    private static double min;
+    private static double ave;
+    private static double sum;
+    private static int count;
+
+    public DataSet() {
+        max = 0;
+        min = 0;
+        ave = 0;
+        sum = 0;
+        count = 0;
+    }
+
+    public DataSet(Measurable x) {
+        count++;
+        sum += x.getMeasure();
+        if (count == 1) {
+            max = x.getMeasure();
+            min = x.getMeasure();
+        } else {
+            if (x.getMeasure() > max)
+                max = x.getMeasure();
+            if (x.getMeasure() < min)
+                min = x.getMeasure();
+        }
+        ave = sum / count;
+    }
+
+    public double getMax() {
+        return max;
+    }
+
+    public double getMin() {
+        return min;
+    }
+
+    public double getAve() {
+        return ave;
+    }
+}
+
