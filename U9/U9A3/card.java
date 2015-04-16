0c8b8472116a3154acf0e0a54f278672509077d4
Anthony Pizzimenti <pizzimentianthony@gmail.com>
2015-04-15 20:28:14 -0500

    formatting

--- U9/U9A3/card.java
+++ U9/U9A3/card.java
@@ -6,59 +6,59 @@
 /* ---------------------- */
 
 public class card implements Comparable {
-
-	private String suit;
-	private int value;
+    
+    private String suit;
+    private int value;
     private String name = "";
-	private int points;
-
-	public card (String a, int b) {
-		suit = a;
-		value = b;
+    private int points;
+    
+    public card (String a, int b) {
+        suit = a;
+        value = b;
         setPoints();
         setValue();
-	}
-
-	public void setPoints() {
+    }
+    
+    public void setPoints() {
         if (suit.equals("Hearts")) {
             points = 1;
         } else if (suit.equals("Spades") && value == 12) {
             points = 13;
         }
-	}
-
-	public void setValue() {
-
-		switch (value) {
-			case 11: name = "Jack";
-				break;
-			case 12: name = "Queen";
-				break;
-			case 13: name = "King";
-				break;
-			case 14: name = "Ace";
-				break;
-			default: name = "" + value;
-				break;
-		}
-	}
-
-	public String getSuit() {
-		return suit;
-	}
-
+    }
+    
+    public void setValue() {
+        
+        switch (value) {
+            case 11: name = "Jack";
+            break;
+            case 12: name = "Queen";
+            break;
+            case 13: name = "King";
+            break;
+            case 14: name = "Ace";
+            break;
+            default: name = "" + value;
+            break;
+        }
+    }
+    
+    public String getSuit() {
+        return suit;
+    }
+    
     public int getValue() {
         return value;
     }
-
-	public int getPoints() {
-		return points;
-	}
-
-	public int compareTo(Object obj) {
+    
+    public int getPoints() {
+        return points;
+    }
+    
+    public int compareTo(Object obj) {
         int total = 0;
         card def = (card)obj;
-
+        
         if (getSuit().equals(def.getSuit())) {
             if (getValue() > def.getValue()) {
                 total = 1;
@@ -68,12 +68,12 @@ public class card implements Comparable {
                 total = -1;
             }
         } else {
-			total = 1;
-		}
-
+            total = 1;
+        }
+        
         return total;
-	}
-
+    }
+    
     public String toString() {
         return name + " of " + suit;
     }
--- U9/U9A3/trick.java
+++ U9/U9A3/trick.java
@@ -8,50 +8,50 @@
 import java.util.*;
 
 public class trick {
-
-	private ArrayList<card> cards = new ArrayList<card>();
-	private int points;
-
-	public trick(ArrayList<card> x) {
-		cards = x;
+    
+    private ArrayList<card> cards = new ArrayList<card>();
+    private int points;
+    
+    public trick(ArrayList<card> x) {
+        cards = x;
         setTotalPoints();
-	}
-
-	public void setTotalPoints() {
+    }
+    
+    public void setTotalPoints() {
         for (int i = 0; i < cards.size(); i++) {
             card x = cards.get(i);
             points += x.getPoints();
         }
-	}
-
+    }
+    
     public int getTotalPoints() {
         return points;
     }
-
+    
     public String getWinner() {
-		String winner = null;
+        String winner = null;
         card x = cards.get(0);
-
+        
         for (int i = 1; i < cards.size(); i++) {
             Object y = cards.get(i);
             int except = x.compareTo(y);
-
+            
             if (except == 1) {
-				continue;
-			} else if (except == -1) {
-				x = (card)y;
-			}
+                continue;
+            } else if (except == -1) {
+                x = (card)y;
+            }
         }
         return x.toString();
     }
-
-	public String toString() {
-		String names = null;
-
-		for (int i = 0; i < cards.size(); i++) {
-			card x = cards.get(i);
-			names += x.toString() + "\n";
-		}
-		return names;
-	}
+    
+    public String toString() {
+        String names = null;
+        
+        for (int i = 0; i < cards.size(); i++) {
+            card x = cards.get(i);
+            names += x.toString() + "\n";
+        }
+        return names;
+    }
 }
